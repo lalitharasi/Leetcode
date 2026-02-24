f9e60f1db040c6d94c0b7dd2a10b2fb022cc64cd
Lalitha Bala Rasi <lalithabalarasi@gmail.com>
2026-02-25 03:07:42 +0530

    Add files via upload

---
+++ Palin.java
@@ -0,0 +1,20 @@
+class Palin {
+    public boolean isPalindrome(int x) {
+        
+        // Negative numbers are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversedHalf
+        // For odd digits: x == reversedHalf / 10
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+}
